b813f404af24ecf6ab44314d964977a14b56b4aa
Azamat Dhuzumaliev <juzumaliev@gmail.com>
2022-05-19 11:27:40 +0600

    code added to board.js

--- board.ts
+++ board.ts
@@ -1,2 +1,23 @@
+const white: string = "  ";
+const black: string = "██";
 
+for (let i:number = 0; i <= 6; i++) {
+    let line:string = "";
+    for (let j:number = 0; j <=8; j++) {
+        if (i % 2) {
+            if (j % 2) {
+                line += black;
+            } else {
+                line += white;
+            }
+        } else {
+            if (j % 2) {
+                line += white;
+            } else {
+                line += black;
+            }
+        }
+    }
+    console.log(line);
+}
 
